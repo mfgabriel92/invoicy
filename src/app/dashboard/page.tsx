435db32c7c7b4a53e0905bc16ccaffe9053ffb7a
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2024-12-04 12:03:18 +0800

    feat: dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,51 @@
+﻿import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { PlusCircle } from "lucide-react";
+
+export default function Dashboard() {
+  return (
+    <main className="mx-auto flex h-full max-w-5xl flex-col gap-8 p-8">
+      <div className="flex items-end justify-between">
+        <h1 className="text-5xl font-bold">Dashboard</h1>
+        <Button asChild variant="ghost">
+          <Link href="/invoices/new">
+            <PlusCircle />
+            New Invoice
+          </Link>
+        </Button>
+      </div>
+      <hr />
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px] text-left">Date</TableHead>
+            <TableHead className="text-left">Customer</TableHead>
+            <TableHead className="text-left">E-mail</TableHead>
+            <TableHead className="text-left">Status</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="text-left">04/12/2024</TableCell>
+            <TableCell className="text-left">John Doe</TableCell>
+            <TableCell className="text-left">johndoe@gmail.com</TableCell>
+            <TableCell className="text-left">
+              <Badge color="primary">Open</Badge>
+            </TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </main>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +10,53 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT:"hsl(var(--popover))"",
+          foreground:"hsl(var(--popover-foreground))"",
+        },
+        primary: {
+          DEFAULT"hsl(var(--primary))")",
+          foreground"hsl(var(--primary-foreground))")",
+        },
+        secondary: {
+          DEFAUL"hsl(var(--secondary))"))",
+          foregroun"hsl(var(--secondary-foreground))"))",
+        },
+        muted: {
+          DEFAU"hsl(var(--muted))"d))",
+          foregrou"hsl(var(--muted-foreground))"d))",
+        },
+        accent: {
+          DEFA"hsl(var(--accent))"nt))",
+          foregro"hsl(var(--accent-foreground))"nd))",
+        },
+        destructive: {
+          DEF"hsl(var(--destructive))"ive))",
+          foregr"hsl(var(--destructive-foreground))"und))",
+        },
+        b"hsl(var(--border))"rder))",
+        "hsl(var(--input))"nput))",
+       "hsl(var(--ring))"ring))",
+        chart: {
+   "1"  "hsl(var(--chart-1))"rt-1))",
+   "2"  "hsl(var(--chart-2))"rt-2))",
+   "3"  "hsl(var(--chart-3))"rt-3))",
+   "4"  "hsl(var(--chart-4))"rt-4))",
+   "5"  "hsl(var(--chart-5))"rt-5))",
+        },
+      },
+      borderRadius: {
+   "var(--radius)"-radius)",
+   "calc(var(--radius) - 2px)") - 2px)",
+   "calc(var(--radius) - 4px)") - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcs-animate")],
 } satisfies Config;
